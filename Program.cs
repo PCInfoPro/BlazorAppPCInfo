7c2ea40c33fd811b9ec6c6b2b12603efffdf4212
fmdev98 <fmdev1998@outlook.com>
2024-10-24 23:57:39 +0200

    Inicios de nueva pantalla y añadido proyecto compartido para datos

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 using BlazorAppPCInfo.Components;
+using INFOPC.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Agregar cliente HTTP
+builder.Services.AddHttpClient<ComputerService>(client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5175/api"); // URL base de tu API
+});
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient();
+builder.Services.AddServerSideBlazor();
+
+// builder.Services.AddScoped<ComputerService>();
+
+
+
+
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(new Microsoft.AspNetCore.Mvc.AutoValidateAntiforgeryTokenAttribute());
+});
 
 var app = builder.Build();
 
@@ -17,6 +33,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// app.UseEndpoints(endpoints =>
+//     {
+//         endpoints.MapRazorPages();
+//         endpoints.MapBlazorHub();
+//         endpoints.MapFallbackToPage("/_Host");
+//     });
 
 app.UseHttpsRedirection();
 
